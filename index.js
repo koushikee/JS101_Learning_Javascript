fe642a44e1ade8e04ec796d1ce44189c67454d0a
koushikee <121329133+koushikee@users.noreply.github.com>
2022-12-31 19:59:48 +0530

    first assignment of variables

---
+++ index.js
@@ -0,0 +1,8 @@
+let a= 10;
+console.log(a);
+
+let name= "name";
+console.log(name);
+
+console.log(typeof(a));
+console.log(typeof(name));
---
+++ problem2.js
@@ -0,0 +1,6 @@
+let name= "Koushikee";
+console.log(name);
+name= "Karunamoy";
+console.log(name);
+name= "Shefali";
+console.log(name);
